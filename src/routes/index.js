d6a51cafbf3be166a104ef83631b28dbfca96575
lu1sfer <lfchaparro37@misena.edu.co>
2023-06-26 16:17:11 -0500

    Update dependencies and add dockerFiles

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ const router= express.Router();
 router.get('/',(req,res)=>{
     //res.sendFile(path.join(__dirname, 'views/index.html'));
     //eje: http://localhost:3000/?name=juan //envia el parametro name=juan al servidor
-    res.render('index.html', { title: 'Bienvenido a mi pagina', query : req.query.name }); //renderiza el archivo index.html que esta en la carpeta views y le envia un objeto con el titulo 
+    res.render('index.html', { title: 'Welcome', query : req.query.name }); //renderiza el archivo index.html que esta en la carpeta views y le envia un objeto con el titulo 
 })
 
 router.all('/info', (req, res,) => { //all es para que acepte cualquier metodo http
